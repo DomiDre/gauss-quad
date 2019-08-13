0d0bdf237c41234713e3def4ff553f4b3afb559d
DomiDre <homiedomi@gmail.com>
2019-08-13 19:32:39 +0200

    Implement Gauss Legendre and Hermite Quadrature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-pub mod hermite;
-pub mod legendre;
+#![allow(dead_code)]
+#![allow(unused_imports)]
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use]
+extern crate assert_float_eq;
+
+use nalgebra::{Dynamic, Matrix, VecStorage};
+pub type DMatrixf64 = Matrix<f64, Dynamic, Dynamic, VecStorage<f64, Dynamic, Dynamic>>;
+pub use std::f64::consts::PI;
+
+pub mod gaussian_quadrature;
+pub use gaussian_quadrature::{QuadratureRule, DefiniteIntegral, ImproperIntegral};
+
+pub mod hermite;
+pub use hermite::GaussHermite;
+
+pub mod legendre;
+pub use legendre::GaussLegendre;
