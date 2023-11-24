343e27fe9f170998f4d3cce1dcc8af90e77516df
Johanna Sörngård <jsorngard@gmail.com>
2023-11-24 16:11:11 +0100

    Panic when alpha or beta == -1, not just if they're smaller

--- src/jacobi/mod.rs
+++ src/jacobi/mod.rs
@@ -58,9 +58,9 @@ impl GaussJacobi {
     /// See Gil, Segura, Temme - Numerical Methods for Special Functions
     ///
     /// # Panics
-    /// Panics if degree of quadrature is smaller than 2, or if alpha or beta are smaller than -1
+    /// Panics if `degree` is smaller than 2, or if `alpha` or `beta` are smaller than or equal to -1.
     pub fn new(deg: usize, alpha: f64, beta: f64) -> GaussJacobi {
-        if alpha < -1.0 || beta < -1.0 {
+        if alpha <= -1.0 || beta <= -1.0 {
             panic!("Gauss-Jacobi quadrature needs alpha > -1.0 and beta > -1.0");
         }
         if deg < 2 {
