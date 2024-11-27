27fff47f3a3d4e503592226edd7a4392f2f82acc
Johanna Sörngård <jsorngard@gmail.com>
2024-11-27 11:32:36 +0100

    Add tests of error branch in par_new

--- src/chebyshev/mod.rs
+++ src/chebyshev/mod.rs
@@ -344,4 +344,11 @@ mod test {
         assert_abs_diff_eq!(rule1.par_integrate(0.0, 2.0, |x| x), PI);
         assert_abs_diff_eq!(rule2.par_integrate(-1.0, 1.0, |x| x * x), PI / 8.0);
     }
+
+    #[cfg(feature = "rayon")]
+    #[test]
+    fn check_par_error() {
+        assert!(GaussChebyshevFirstKind::new(0).is_err());
+        assert!(GaussChebyshevSecondKind::new(0).is_err());
+    }
 }
