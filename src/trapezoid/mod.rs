fc60b130e9f8652345eb96a63d863c478dbfdc56
Johanna Sörngård <jsorngard@gmail.com>
2024-12-30 17:36:46 +0100

    Similar error message

--- src/trapezoid/mod.rs
+++ src/trapezoid/mod.rs
@@ -195,10 +195,7 @@ impl TrapezoidError {
 
 impl std::fmt::Display for TrapezoidError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "the degree of the Trapezoid rule must be greater than 1."
-        )
+        write!(f, "the degree of the Trapezoid rule must be at least 2.")
     }
 }
 
