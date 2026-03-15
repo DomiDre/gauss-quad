24a2403ec234159a516ac18d9cb56635b860a2ad
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-03-15 15:40:07 +0100

    Add info about the parallel integration function to the crate docs (#168)
    * Document the trait bounds for `par_integrate`
    
    * Sentence clarity
    
    * Move doc text
    
    * Correct terminology

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,7 @@
 //! ## Passing functions to quadrature rules
 //!
 //! The `integrate` method takes any integrand that implements the [`FnMut(f64) -> f64`](FnMut) trait, i.e. functions of
-//! one `f64` parameter.
+//! one `f64` parameter that can modify internal state.
 //!
 //! ```
 //! # use gauss_quad::legendre::GaussLegendre;
@@ -158,6 +158,7 @@
 //! let integral_x_cubed = quad.integrate(left_bound, right_bound, x_cubed);
 //! assert_abs_diff_eq!(integral_x_cubed, 1.0 / 4.0);
 //! ```
+//! The parallel `par_integrate` methods instead take a function that can't modify any state, [`Fn(f64) -> f64`](Fn), and is [`Sync`] to enable parallel evaluation.
 //!
 //! ## Double integrals
 //!
