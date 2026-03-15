8b44ea6a598c7657d526114e2f13b9aeb49dba48
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-03-15 20:59:37 +0100

    Don't create the `new_unchecked` function now (#179)

--- src/data_api.rs
+++ src/data_api.rs
@@ -59,23 +59,6 @@ impl FiniteAboveNegOneF64 {
         }
     }
 
-    /// Creates a new `FiniteAboveNegOneF64` without checking the value.
-    ///
-    /// The caller must ensure that the value is finite and greater than -1.0 as well as not [`NAN`](f64::NAN).
-    /// If these requirements are not met this crate will produce invalid results, and may crash.
-    ///
-    /// # Panics
-    ///
-    /// Panics in debug mode if given an input that is infinite, NAN, or less than or equal to -1.
-    #[inline]
-    pub const fn new_unchecked(value: f64) -> Self {
-        debug_assert!(
-            Self::new(value).is_some(),
-            "value must be greater than -1.0, finite, and not NAN"
-        );
-        Self(value)
-    }
-
     /// Returns the inner `f64` value of the `FiniteAboveNegOneF64`.
     #[inline]
     pub const fn get(&self) -> f64 {
@@ -663,9 +646,6 @@ mod tests {
         assert!(FiniteAboveNegOneF64::new(f64::NAN).is_none());
         assert!(FiniteAboveNegOneF64::new(f64::INFINITY).is_none());
         assert!(FiniteAboveNegOneF64::new(f64::NEG_INFINITY).is_none());
-
-        assert_eq!(FiniteAboveNegOneF64::new_unchecked(0.0).get(), 0.0);
-        assert_eq!(FiniteAboveNegOneF64::new_unchecked(-0.5).get(), -0.5);
     }
 
     #[test]
