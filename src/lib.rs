7d6269fabebc8047e1c4fb2b3452a358a1eb655f
Johanna Sörngård <jsorngard@gmail.com>
2026-03-15 15:36:59 +0100

    Sentence clarity

--- src/lib.rs
+++ src/lib.rs
@@ -132,7 +132,7 @@
 //!
 //! The `integrate` method takes any integrand that implements the [`FnMut(f64) -> f64`](FnMut) trait, i.e. functions of
 //! one `f64` parameter that can modify internal state.
-//! The parallel `par_integrate` functions take a [`Fn(f64) -> f64`](Fn) which is [`Sync`] to enable parallel evaluation.
+//! The parallel `par_integrate` functions take a function that can't modify any state, [`Fn(f64) -> f64`](Fn), and is [`Sync`] to enable parallel evaluation.
 //!
 //! ```
 //! # use gauss_quad::legendre::GaussLegendre;
