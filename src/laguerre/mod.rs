7a909a086ea16351f4c49716098d101a4ecc1e65
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-03-15 21:53:47 +0100

    Sort the nodes by the `total_cmp()` function and forbid `unwrap()` (#185)
    * Sort the nodes using the `total_cmp` function
    
    * Add change to log
    
    * `cargo fmt`
    
    * `forbid` `unwrap` and `expect` with clippy

--- src/laguerre/mod.rs
+++ src/laguerre/mod.rs
@@ -103,8 +103,7 @@ impl GaussLaguerre {
             )
             .collect();
 
-        node_weight_pairs
-            .sort_unstable_by(|(node1, _), (node2, _)| node1.partial_cmp(node2).unwrap());
+        node_weight_pairs.sort_unstable_by(|(node1, _), (node2, _)| node1.total_cmp(node2));
 
         GaussLaguerre {
             node_weight_pairs,
