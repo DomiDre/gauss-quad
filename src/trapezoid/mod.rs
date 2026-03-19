700e1b99a3ceaa696226347c53c75d36debdc5d1
Johanna Sörngård <jsorngard@gmail.com>
2026-03-19 21:22:20 +0100

    Make tests pass

--- src/trapezoid/mod.rs
+++ src/trapezoid/mod.rs
@@ -145,7 +145,8 @@ mod test {
     #[test]
     fn test_change_degree() {
         let mut quad = Trapezoid::new(1000.try_into().unwrap());
-        quad.change_degree(2000.try_into().unwrap());
-        assert_eq!(quad.degree(), 2000.try_into().unwrap());
+        let new_deg = 2000.try_into().unwrap();
+        quad.change_degree(new_deg);
+        assert_eq!(quad.degree(), new_deg);
     }
 }
