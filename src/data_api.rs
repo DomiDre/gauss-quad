d6b2dccc9e764bf7e837a1cf9d78f12021288daf
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-03-19 21:11:59 +0100

    Add the `zerocopy` traits to the `FiniteAboveNegOneF64` type (#193)

--- src/data_api.rs
+++ src/data_api.rs
@@ -24,6 +24,15 @@ pub type Weight = f64;
 
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "zerocopy",
+    derive(
+        zerocopy::KnownLayout,
+        zerocopy::Immutable,
+        zerocopy::FromZeros,
+        zerocopy::IntoBytes
+    )
+)]
 /// A wrapper around an [`f64`] that ensures the value is greater than -1.0, finite, and not [`NAN`](f64::NAN).
 pub struct FiniteAboveNegOneF64(f64);
 
