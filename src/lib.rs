dee8242acf17ab6c53dae6bb45525539b2e48f6b
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-03-19 21:46:19 +0100

    Make the `KnownLayout` text a doc-link (#195)

--- src/lib.rs
+++ src/lib.rs
@@ -188,7 +188,7 @@
 //! `rayon`: enables a parallel version of the `integrate` function on the quadrature rule structs. Can speed up integration if evaluating the integrand is expensive (takes ≫100 µs).
 //! The [`rayon`] crate depends on the standard library, so this also enables the `std` feature.
 //!
-//! `zerocopy`: imlements the `KnownLayout` trait from the [`zerocopy`] crate for the quadrature rule structs.
+//! `zerocopy`: imlements the [`KnownLayout`](zerocopy::KnownLayout) trait from the [`zerocopy`] crate for the quadrature rule structs.
 //!
 //! One of the below features must be enabled:
 //!
